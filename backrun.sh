4a6ab0cb47898b18defbd6556ae8d0907e6d513d
安达儿 <44111917+fidshu@users.noreply.github.com>
2025-08-26 13:53:56 +0800

    HW_SP

---
+++ backrun.sh
@@ -0,0 +1,2 @@
+python featup/train_upsampler_MAE.py > /mnt/sda/pai-hdd/bknian/CLIP/SimFeatUp-main/MAE.log 2>&1 &&
+python featup/train_upsampler.py > /mnt/sda/pai-hdd/bknian/CLIP/SimFeatUp-main/MAE_Normal.log 2>&1 &
